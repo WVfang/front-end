f740cfb628abb012f522044eb641814d2434c72e
WVfang <woldy1648@gmail.com>
2017-08-22 23:16:18 +0300

    Fixed button-problem in UALP

--- Unimaginable_App_Landing_Page/assets/js/main.js
+++ Unimaginable_App_Landing_Page/assets/js/main.js
@@ -3,6 +3,15 @@ $(document).ready(function () {
         $("body, html").animate({scrollTop: 0}, 1100);
     })
 
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+            $("#btn-to-top").fadeIn();   
+        } else {
+            console.log("scrolling-detected");
+            $("#btn-to-top").fadeOut();
+        }
+    }); 
+
     $("a").click(function () {
         var elementClick = $(this).attr("href");
         var coordinates = $(elementClick).offset();
@@ -11,26 +20,6 @@ $(document).ready(function () {
         $("body, html").animate({ scrollTop: destination }, 1100);
         
         return false; 
-    });
-
-    $(window).scroll(function() {
-        if($(this).scrollTop() > 250) {
-            $("#btn-to-top").fadeIn();      
-        } else {
-            $("#btn-to-top").fadeOut();
-        }
-    });
-
-    $("#btn-to-top").hover(
-        function() {
-            $(this)
-                .css("background-color", "rgba(255,255,255,0.25)")
-                .css("transition", "1s");
-        },
-        function() {
-            $(this)
-                .css("background-color", "transparent")
-                .css("transition", "1s");
-        }, 
-    );
+    }); 
 });
+
