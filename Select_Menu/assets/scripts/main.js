1321ceb967c9d314d29eeac163a96f5c9f6d1567
WVfang <woldy1648@gmail.com>
2017-08-22 21:12:00 +0300

    Finished Task4 v.1.0 :)

--- Select_Menu/assets/scripts/main.js
+++ Select_Menu/assets/scripts/main.js
@@ -1,18 +1,34 @@
 $(document).ready(function (){
-	$("#friend-options").hide ();
+	/* Создание списка с информации */
+	var persons = [["Jenny Hess", "assets\\images\\Cancer.jpg"],
+	 				["Elliot Fu", "assets\\images\\Leo.jpg"],
+	 				["Stevie Feliciano", "assets\\images\\Gemini.jpg"],
+	 				["Christian", "assets\\images\\Pisces.jpg"],
+	 				["Matt","assets\\images\\Scorpio.jpg"]];
 
-	$(".select-menu").hover (
-		function() {
-			$("#friend-options").show();
-			$(".select-menu").addClass("active-border");
-		},
-		function() {
-			$("#friend-options").hide();
-			$(".select-menu").removeClass("active-border");
+	for(var i = 0; i < persons.length; i++) {
+		$("#friend-total-options").append("<li class=\"friend-option\"><img src=\"" + 
+			persons[i][1] + "\"></img><div>" + persons[i][0] + "</div></li>");
+	}
+	/* Изначальное скрытие списка */
+	$("#friend-total-options").hide ();
+	/* Выдвижение/задвижение списка при клике на него/вне списка */
+	$("body").click (
+		function(event) {
+			if($(event.target).closest("#select-menu").length) {
+				$("#friend-total-options").show();
+				$("#select-menu")
+					.css("border-color", "#87ceeb");
+			}
+			else {
+				$("#friend-total-options").hide();
+				$("#select-menu")
+					.css("border-color", "#d3d3d3");
+			}
 		}
 	);
-
-	$("#friend-options li").hover (
+	/* Смена фона при наведении на элемент списка и выбор элемента при клике на него */
+	$(".friend-option").hover (
 		function() {
 			$(this).addClass("active-background");
 		},
@@ -20,8 +36,8 @@ $(document).ready(function (){
 			$(this).removeClass("active-background");
 		}
 	).on("click", function() {
-		$(".select-menu div:first").removeAttr("id").addClass("friend-option");
-		$(".select-menu div:first").children().not(".fa").remove();
-		$(this).children().clone().appendTo($(".select-menu div:first"));
+		$("#select-menu div:first").removeAttr("id").addClass("friend-option");
+		$("#select-menu div:first").children().not(".fa").remove();
+		$(this).children().clone().appendTo($("#select-menu div:first"));
 	})
 });
