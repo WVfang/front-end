039ac4d388f47ac2985793f40839c72977467113
WVfang <woldy1648@gmail.com>
2017-09-05 19:59:17 +0300

    Started RegExp tasks

---
+++ RegExp/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RegExp</title>
+</head>
+<body>
+	<style>
+		input {
+			display: block;
+			margin-bottom: 10px;
+		}
+	</style>
+	<form>
+		<input id="input-id" type="" name="input-ip">
+		<input id="input-url" type="" name="input-url">
+		<input id="input-email" type="" name="input-email">
+		<input id="input-date" type="" name="input-date">
+		<input id="input-time" type="" name="input-time">
+		<button>Commit</button>
+	</form>
+
+
+</body>
+</html>
